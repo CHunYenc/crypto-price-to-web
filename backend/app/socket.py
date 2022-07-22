7e1ce9ad45547d0c063f6a38338a58c87d56275b
CHunYenc <yuhnoesty0402@hotmail.com.tw>
2022-07-23 02:54:21 +0800

    feat: server emit data to client every 2s.

--- backend/app/socket.py
+++ backend/app/socket.py
@@ -1,5 +1,7 @@
 import json
 from os import symlink
+import time
+
 from flask_socketio import Namespace, emit, send
 from flask import current_app
 from app import redis as r
@@ -11,9 +13,11 @@ class MyCryptoPriceNamespace(Namespace):
         exchange_list = r.keys('CRYPTO_*')
         current_app.logger.info(exchange_list)
         emit("get_exchange", [x.decode() for x in exchange_list])
+        self.status = False
 
     def on_disconnect(self):
         current_app.logger.info('disconnect.')
+        self.status = False
 
     def on_get_symbol(self, data):
         symbol_list = r.get(data["data"])
@@ -24,7 +28,12 @@ class MyCryptoPriceNamespace(Namespace):
         emit("get_symbol", result)
 
     def on_get_symbol_data(self, data):
-        exchange = str.upper(f"{data['exchange']}")
-        queryset = r.get(f'{exchange}')
-        result = json.loads(queryset)
-        emit("get_symbol_data", result[data['symbol']], broadcast=True)
+        self.status = True
+        self.exchange = data['exchange'].upper()
+        self.symbol = data['symbol'].upper()
+        while self.status:
+            queryset = r.get(f'{self.exchange}')
+            result = json.loads(queryset)
+            emit("get_symbol_data", result[self.symbol])
+            time.sleep(2)
+
--- backend/app/static/price_s.js
+++ backend/app/static/price_s.js
@@ -1,9 +1,9 @@
 var app = new Vue({
-  el: "#app",
-  delimiters: ["[[", "]]"],
+  el: '#app',
+  delimiters: ['[[', ']]'],
   data() {
     return {
-      text: "Hello Vue2",
+      text: 'Hello Vue2',
       socket: null,
       // select exchange
       select_exchange_items: [],
@@ -17,38 +17,45 @@ var app = new Vue({
       select_symbol_disabled: true,
       // exchange and symbol data
       symbol_data: {},
-    };
+      current_price: null
+    }
   },
   created() {
-    var domain = "http://localhost:5000/ws";
-    this.socket = io.connect(domain);
-    this.socket.on("connect", () => {
-      this.select_exchange_disabled = false;
-    });
-    this.socket.on("get_exchange", (result) => {
-      this.select_exchange_items = result;
-    });
-    this.socket.on("get_symbol", (result) => {
-      this.select_symbol_items = result;
-      this.select_symbol_disabled = false;
-    });
-    this.socket.on("get_symbol_data", (result) => {
-      this.symbol_data = result;
-    });
+    var domain = `ws://${window.location.hostname}:${window.location.port}/ws`
+    // console.log(domain);
+    this.socket = io.connect(domain)
+    this.socket.on('connect', () => {
+      this.select_exchange_disabled = false
+    })
+    this.socket.on('get_exchange', (result) => {
+      this.select_exchange_items = result
+    })
+    this.socket.on('get_symbol', (result) => {
+      this.select_symbol_items = result
+      this.select_symbol_disabled = false
+    })
+    this.socket.on('get_symbol_data', (result) => {
+      console.log('get_symbol_data')
+      this.symbol_data = result
+      if (this.select_exchange.includes('BINANCE')) {
+        this.current_price = this.symbol_data.info.lastPrice
+      }
+      document.title = `${this.current_price} - ${this.select_exchange} - ${this.select_symbol}`
+    })
   },
   methods: {
     select_exchange_method(value) {
       // console.log(value);
-      this.socket.emit("get_symbol", { data: this.select_exchange });
+      this.socket.emit('get_symbol', { data: this.select_exchange })
     },
     select_data() {
       console.log(
         `select exchange is ${this.select_exchange}, symbol is ${this.select_symbol}.`
-      );
-      this.socket.emit("get_symbol_data", {
+      )
+      this.socket.emit('get_symbol_data', {
         exchange: this.select_exchange,
-        symbol: this.select_symbol,
-      });
-    },
-  },
-});
+        symbol: this.select_symbol
+      })
+    }
+  }
+})
